d654c559d4bd5c8d4c6b7d2fcd502ad015baa71e
hjyanghj <hjyanghj@gmail.com>
2023-02-09 12:43:36 +0800

    Add Api(Azure functions projet .Net 6.0(LTS))，with  Azure function Function1 and WeatherForecastFunction, Call Api from Client

---
+++ Api/WeatherForecastFunction.cs
@@ -0,0 +1,58 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Net;
+using Shared;
+
+namespace Api
+{
+    public class WeatherForecastFunction
+    {
+        [FunctionName("WeatherForecastFunction")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            await Task.Run(() => { });
+            var randomNumber = new Random();
+            var temp = 0;
+
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = temp = randomNumber.Next(-20, 55),
+                Summary = GetSummary(temp)
+            }).ToArray();
+
+            return new OkObjectResult(result);
+        }
+        private static string GetSummary(int temp)
+        {
+            var summary = "Mild";
+
+            if (temp >= 32)
+            {
+                summary = "Hot";
+            }
+            else if (temp <= 16 && temp > 0)
+            {
+                summary = "Cold";
+            }
+            else if (temp <= 0)
+            {
+                summary = "Freezing";
+            }
+
+            return summary;
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,17 @@
-* Client: Blazor WebAssembly .Net 6.0
+* Client: Blazor WebAssembly(.Net 6.0)
+* 加入 Azure Static Web App workflow(Azure Portal)
+
+## 加入 Shared
+* 加入類別庫專案 Shared(.Net Standard 2.0)
+    * 加入 WeatherForecast 類別，讓 Client 與 Api 專案都可以使用。
+## 加入 Api
+* 加入 Azure functions 專案 Api(.Net 6.0(LTS))
+    * launchSettings.json>profiles.Api.commandLineArgs 的值由 "--port 7199" 改成 "start --cors * --port 7199"(讓Client　呼叫 Api 時沒有CORS問題)
+    * 加入 Azure 函數 Function1 與 WeatherForecastFunction
+
+## Client 呼叫 Api
+* Client.wwwroot.appsettings.Development.json 裡面設 "API_Prefix": "http://localhost:7199"，讓local端執行時知道如何呼叫Api。
+* Client.Program.cs 裡面，`BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)`改成`BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress)`，讓cloacal開發與雲端佈建時都行得通。
+* Client 裡面新增razor 元件 CallFunction1,razor，裡面呼叫 Api 的 Function1
+* Client 的 Index.razor 使用了 CallFunction1,razor
+* Client 的 FetchData.razor 頁面，修改成呼叫 Api 的 WeatherForecastFunction。
